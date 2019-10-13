c5b3d1f26616ad0fa0aeadcc3233c11086c4f7cb
Greg D <gregorydesmarais@gmail.com>
2019-10-13 19:10:27 -0400

    Can now receive data on the server.  Spun up basic home page. Changed default port on server.js for testing purposes.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 const friends = [{
-        "name": "Ken",
-        "photo": "https://i.ytimg.com/vi/-14W5XTqL5U/hqdefault.jpg",
-        "scores": [5, 6, 2, 6, 8, 7, 2, 5, 5, 1]
+        name: "Ken",
+        photo: "https://i.ytimg.com/vi/-14W5XTqL5U/hqdefault.jpg",
+        scores: [5, 6, 2, 6, 8, 7, 2, 5, 5, 1]
     },
     {
-        "name": "Ryu",
-        "photo": "https://i.ytimg.com/vi/o7mLBbSA578/hqdefault.jpg",
-        "scores": [9, 3, 4, 3, 9, 1, 1, 8, 7, 6]
+        name: "Ryu",
+        photo: "https://i.ytimg.com/vi/o7mLBbSA578/hqdefault.jpg",
+        scores: [9, 3, 4, 3, 9, 1, 1, 8, 7, 6]
     },
     {
-        "name": "Jill",
-        "photo": "https://giantbomb1.cbsistatic.com/uploads/scale_small/7/70497/1210176-jill_mvc2a.jpg",
-        "scores": [9, 8, 8, 6, 3, 3, 6, 2, 2, 3]
+        name: "Jill",
+        photo: "https://giantbomb1.cbsistatic.com/uploads/scale_small/7/70497/1210176-jill_mvc2a.jpg",
+        scores: [9, 8, 8, 6, 3, 3, 6, 2, 2, 3]
     },
     {
-        "name": "Guile",
-        "photo": "https://i.ytimg.com/vi/79nKcXzWd5c/maxresdefault.jpg",
-        "scores": [3, 7, 5, 8, 6, 9, 1, 6, 7, 3]
+        name: "Guile",
+        photo: "https://i.ytimg.com/vi/79nKcXzWd5c/maxresdefault.jpg",
+        scores: [3, 7, 5, 8, 6, 9, 1, 6, 7, 3]
     },
     {
-        "name": "Chun-Li",
-        "photo": "https://i.ytimg.com/vi/wvduT7JqnpI/hqdefault.jpg",
-        "scores": [4, 7, 2, 2, 7, 9, 7, 9, 8, 7]
+        name: "Chun-Li",
+        photo: "https://i.ytimg.com/vi/wvduT7JqnpI/hqdefault.jpg",
+        scores: [4, 7, 2, 2, 7, 9, 7, 9, 8, 7]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,14 +2,21 @@ var friends = require("../data/friends.js");
 
 module.exports = function(app) {
 
-
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
 
     app.post("/api/friends", function(req, res) {
         var newFriend = req.body;
-
+        for (x in newFriend.scores) {
+            newFriend.scores[x] = parseInt(newFriend.scores[x]);
+        }
+        friends.push({
+            name: newFriend.name,
+            photo: newFriend.photo,
+            scores: newFriend.scores
+        })
         console.log(newFriend);
+        res.json(newFriend);
     });
 }
