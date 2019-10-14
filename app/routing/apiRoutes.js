66cd7d47ff5ea33f69f7252551067e102235df09
Greg D <gregorydesmarais@gmail.com>
2019-10-14 11:34:23 -0400

    Survey: Added validation code.  Modal now displays friend data.  apiRoutes: Completed friend matching code.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,10 @@
 var friends = require("../data/friends.js");
 
+//Adding method to easily find lowest value of an array.  Adopted from https://johnresig.com/blog/fast-javascript-maxmin/
+Array.min = function(array) {
+    return Math.min.apply(Math, array);
+}
+
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
@@ -11,12 +16,28 @@ module.exports = function(app) {
         for (x in newFriend.scores) {
             newFriend.scores[x] = parseInt(newFriend.scores[x]);
         }
+        const compareFriend = [];
+        let sum = 0;
+        for (x in friends) {
+            // console.log(newFriend.scores + "\n" + friends[x].scores)
+            for (y in friends[x].scores) {
+                sum += Math.abs(newFriend.scores[y] - friends[x].scores[y]);
+                // console.log((Math.abs(newFriend.scores[y] - friends[x].scores[y])) + "\nsum: " + sum);
+            }
+            compareFriend.push(sum)
+            sum = 0;
+        }
+        // console.log(compareFriend);
+        let match = compareFriend.indexOf(Array.min(compareFriend));
+        console.log(friends[match].name + " | " + friends[match].photo)
+        res.json({
+            name: friends[match].name,
+            photo: friends[match].photo
+        });
         friends.push({
             name: newFriend.name,
             photo: newFriend.photo,
             scores: newFriend.scores
         })
-        console.log(newFriend);
-        res.json(newFriend);
     });
 }
